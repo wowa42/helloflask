0eb501f9b77138dbaa4933338197603e8f19493a
Wolfgang <wolfgang@walznet.de>
2018-11-21 10:20:16 +0100

    complete directory

--- app.py
+++ app.py
@@ -55,3 +55,59 @@ def using_templates():
     return render_template('hello.html')
 
 
+# JINJA TEMPLATES
+@app.route('/watch')
+def movies_2017():
+    movie_list = ['autopsy of jane doe',
+                  'neon demon',
+                  'ghost in a shell',
+                  'kong: skull island',
+                  'john wick 2',
+                  'spiderman - homecoming']
+
+    return render_template('movies.html',
+                           movies=movie_list,
+                           name='Harry')
+
+
+@app.route('/tables')
+def movies_plus():
+    movies_dict = {'autopsy of jane doe': 02.14,
+                   'neon demon': 3.20,
+                   'ghost in a shell': 1.50,
+                   'kong: skull island': 3.50,
+                   'john wick 2': 02.52,
+                   'spiderman - homecoming': 1.48}
+
+    return render_template('table_data.html',
+                           movies=movies_dict,
+                           name='Sally')
+
+
+@app.route('/filters')
+def filter_data():
+    movies_dict = {'autopsy of jane doe': 02.14,
+                   'neon demon': 3.20,
+                   'ghost in a shell': 1.50,
+                   'kong: skull island': 3.50,
+                   'john wick 2': 02.52,
+                   'spiderman - homecoming': 1.48}
+
+    return render_template('filter_data.html',
+                           movies=movies_dict,
+                           name=None,
+                           film='a christmas carol')
+
+
+@app.route('/macros')
+def jinja_macros():
+    movies_dict = {'autopsy of jane doe': 02.14,
+                   'neon demon': 3.20,
+                   'ghost in a shell': 1.50,
+                   'kong: skull island': 3.50,
+                   'john wick 2': 02.52,
+                   'spiderman - homecoming': 1.48}
+
+    return render_template('using_macros.html',
+                           movies=movies_dict)
+
